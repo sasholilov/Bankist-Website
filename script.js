442d7467271f4242373d11fbd9330c3efaf13594
sasholilov <alex_xld@abv.bg>
2023-01-17 23:04:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -131,4 +131,29 @@ btnSrollTo.addEventListener('click', function (e) {
   // });
 
   section1.scrollIntoView({ behavior: 'smooth' });
+  //});
+  //const h1 = document.querySelector('h1');
+  //const alertH1 = function (e) {
+  //  alert('addEventListener: Great! You are reading the heading');
+  //};
+  //
+  //h1.addEventListener('mouseenter', alertH1);
+  //
+  //setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 });
+
+//const randomInt = (min, max) =>
+//  Math.floor(Math.random() * (max - min + 1) + min);
+//const randomColor = () =>
+//  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+//
+//document.querySelector('.nav__link').addEventListener('click', function (e) {
+//  this.style.backgroundColor = randomColor();
+//  console.log('LINK', e.target, e.currentTarget);
+//  console.log(e.currentTarget === this);
+//});
+//
+//document.querySelector('.nav').addEventListener('click', function (e) {
+//  this.style.backgroundColor = randomColor();
+//  console.log('NAV', e.target, e.currentTarget);
+//});
